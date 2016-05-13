7afca111bc5a93d469d8aeb14e731945d199c57e
rmezajr <rmezajr@gmail.com>
2016-05-13 12:28:02 -0700

    Added js/background.js

---
+++ js/background.js
@@ -0,0 +1,35 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if(tab.url.indexOf("://www.youtube.com/watch?") > -1) {
+		chrome.pageAction.show(tabId);
+	}
+}
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+	chrome.pageAction.getTitle({
+		"tabId": tab.id
+	}, function(result) {
+		var icon, title;
+		
+		switch(result) {
+			case "expand":
+				icon = "icons/pa_shrink.png";
+				title = "shrink";
+				break;
+			case "shrink":
+				icon = "icons/pa_expand.png";
+				title = "expand";
+		}
+		
+		chrome.pageAction.setTitle({
+			"tabId": tab.id,
+			"title": title
+		});
+		chrome.pageAction.setIcon({
+			"tabId": tab.id,
+			"path": icon
+		});
+		chrome.tabs.executeScript(tab.id, {
+			file: "js/action.js"
+		});
+	});
+});
