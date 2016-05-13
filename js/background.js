8338bc9ac93b81f2863d406b79a725455b7ce081
rmezajr <rmezajr@gmail.com>
2016-05-13 13:37:04 -0700

    FIXED: Added missing parenthesis

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if(tab.url.indexOf("://www.youtube.com/watch?") > -1) {
 		chrome.pageAction.show(tabId);
 	}
-}
+});
 
 chrome.pageAction.onClicked.addListener(function(tab) {
 	chrome.pageAction.getTitle({
